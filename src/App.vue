76e25cf1c6155456c5a189c586ebef1e767a0bdc
lexabass <vedernikov.aleksey.03@gmail.com>
2022-10-16 14:40:26 +0600

    add remove and modal window

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="app">
-    <post-form
+    <h1>Страница с постами</h1>
+    <my-button 
+      @click="showDialog"
+      style="margin: 15px 0;"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
       @create="createPost"/>
+    </my-dialog>
+    
     <post-list 
       :posts="posts"
       @remove="removePost"
@@ -12,28 +22,44 @@
 <script>
 import PostForm from '@/components/PostForm';
 import PostList from '@/components/PostList';
+import MyDialog from './components/UI/MyDialog.vue';
+import MyButton from './components/UI/MyButton.vue';
+import axios from 'axios'
 export default {
   components: {
-    PostList, 
-    PostForm
-  },
+    PostList,
+    PostForm,
+    MyDialog,
+    MyButton
+},
   data() {
     return {
       posts: [
-        {id: 1, title: 'Vue3', body: 'Описание 1'},
-        {id: 2, title: 'Vue2', body: 'Описание 2'},
-        {id: 3, title: 'Vue1', body: 'Описание 3'},
-        {id: 4, title: 'VueEx', body: 'Описание 4'},
+        
       ],
-
+      dialogVisible: false,
+      modificatorValue: ''
     }
   },
   methods: {
     createPost(post) {
       this.posts.push(post)
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts =  this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        this.posts = response.data;
+        console.log(response);
+      } catch (e) {
+        alert('Ошибка')
+      }
     }
   }
 }
